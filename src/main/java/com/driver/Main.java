f74f3d51b75dde63fcbad664499427c79ebd6b30
Tumwesigye Ronnie <tumronald1@gmail.com>
2023-07-02 17:06:43 +0300

    Updated the return

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,22 +1,27 @@
 package com.driver;
 
 public class Main {
+
+    public static class A {
+
+        public  String meth()
+        {
+            return "Invoking method from class A";
+        }
+    }
+
+    public static class B extends A {
+        public  String meth()
+        {
+            return "Method is overridden in Extendend class B";
+        }
+    }
     public static void main(String[] args) {
-        B b = new B();
-        System.out.println(b.meth());
-    }
-  
-}
+        B obj = new B();
+        obj.meth();
 
-class A {
-    public String meth(){
-        return "Invoking method from class A";
+        B obj1 = new B();
+        obj1.meth();
     }
-}
 
-class B extends A{
-    @Override
-    public String meth() {
-        return "Method is overridden in Extendend class B";
-    }
 }
